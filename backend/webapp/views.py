d1f5090c2b41671edd80ff205bddbb50067e475a
Hrishikesh-Padale <padalehrishi07@gmail.com>
2021-03-31 19:24:59 +0530

    adding stocks model

--- backend/webapp/views.py
+++ backend/webapp/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 def index(request):
 	return render(request,'webapp/index.html')
 
+#function to add stock instances of model Stocks
+def add_stock():
+	name = "SBI"
+	price = 371.10
+	change = 4.15
+	percent_change = 1.12
+	volume = 25.339
+	avg_volume = 50.82
+	market_cap = 3.311
+	pe_ratio = 14.28
+	new_stock = Stocks(name=name,price=price,change=change,percent_change=percent_change,
+					   volume=volume,avg_volume=avg_volume,market_cap=market_cap,pe_ratio=pe_ratio)
+
+	new_stock.save()
 	
 
