6640aa633958289166bb736de22de43783da395b
Hrishikesh-Padale <padalehrishi07@gmail.com>
2021-04-08 09:52:24 +0530

    Added Serializer

--- backend/webapp/views.py
+++ backend/webapp/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from webapp.serializers import *
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import *
 import csv
 
 # Create your views here.
 
+@api_view(['GET'])
 def index(request):
-	add_stock()
-	return render(request,'webapp/index.html')
+	add_stock()	#add stock instances
+	objects = Stocks.objects.all()
+	serializer = StocksSerializer(objects,many=True) #serialize 
+	#print(type(serializer.data))
+	return Response(serializer.data) #render the json 
 
 
 #function to add stock instances of model Stocks
